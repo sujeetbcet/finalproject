6cffed789640f0ce3b8a0fcafb02ee4471057a4c
sujeetgreat <sujeet5000@gmail.com>
2015-10-26 15:29:35 +0530

    newly added

--- eclipseSelenium/src/Javapractice/CheckAbstractConstructor.java
+++ eclipseSelenium/src/Javapractice/CheckAbstractConstructor.java
@@ -3,6 +3,7 @@ package Javapractice;
 public class CheckAbstractConstructor extends ConstructorCalling{
 	public static void main(String[] args) {
 		CheckAbstractConstructor rv = new CheckAbstractConstructor();
+		System.out.println(rv);
 	}
 	public CheckAbstractConstructor() {
 		System.out.println("hello");
